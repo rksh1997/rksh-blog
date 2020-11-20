095126a0d524562cdec21b55467d2c390c8ccb67
Rashad Kokash <richardeo112@gmail.com>
2020-11-20 19:44:04 +0200

    Change picture

--- src/components/HeaderImage/index.js
+++ src/components/HeaderImage/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import Link from "gatsby-link";
 
 import Wrapper from "./Wrapper";
-import imgSrc from "../../main2.jpg";
+import imgSrc from "../../main3.jpeg";
 
 function HeaderImage() {
   return (
